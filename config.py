68c09de05cc77a4841ea51eb596075229098590c
hyh_cut@yeah.net <hyh_cut@yeah.net>
2019-02-17 17:56:20 +0800

    完成ssh查看centos服务器内存和CPU功能

---
+++ config.py
@@ -0,0 +1,5 @@
+# 本机
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://monitor:hyh282038@127.0.0.1:3306/Monitor"
+# 阿里云
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://monitor:monitor@39.105.101.210:3306/monitor"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
